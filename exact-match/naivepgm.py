b531f188705ad76d2ea9dd560d2c78a8dc1370cc
lekshmysree <lekshmy.sree@gmail.com>
2017-08-29 23:27:50 -0700

    renaming folder

---
+++ exact-match/naivepgm.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+
+def readgenome(filename):
+    genome=''
+    with open(filename,'r') as f:
+        for line in f:
+            if line[0]!='>':
+                genome+=line.rstrip()
+    return genome
+
+def readfastq(filename):
+    sequences=[]
+    qualities=[]
+    with open(filename,'r') as f:
+        while True:
+            f.readline()
+            seqs=f.readline()
+            f.readline()
+            qual=f.readline()
+            if len(seqs)==0:
+                break
+            sequences.append(seqs)
+            qualities.append(qual)
+    return sequences,qualities
+        
+def gccheck(reads):
+    gc=[0]*101
+    total=[0]*101
+    for read in reads:
+        for i in range(len(read)):
+            if read[i]=='G' or read[i]=='C':
+                gc[i]+=1
+            total[i]+=1
+    for i in range(len(gc)):
+        if total[i]!=0:
+            gc[i]/=float(total[i])
+    return gc
+        
+
+def reversecomplement(p):
+    basecomplement={'A':'T', 'C':'G', 'G':'C', 'T':'A', 'N':'N'}
+    t=''
+    for base in p:
+        t=basecomplement[base]+t
+    return t
+
+
+def naive(p,t):
+    occurences=[]
+    for i in range(len(t)-len(p)+1):
+        match=True
+        for j in range(len(p)):
+            if t[i+j]!= p[j]:
+                match=False
+                break
+        if match:
+            occurences.append(i)
+    return occurences
+
+def naive2mm(p,t):
+    occurences=[]
+    for i in range(len(t)-len(p)+1):
+        match=True
+        num_mismatch=0
+        for j in range(len(p)):
+            if t[i+j]!= p[j]:
+                num_mismatch+=1
+                if num_mismatch>2:
+                   match=False
+                   break
+        if match:
+            occurences.append(i)
+    return occurences
+
+seqs,quals=readfastq('resources/ERR037900_1.first1000.fastq')
+gc=gccheck(seqs)
+print "gc:{}".format(gc)
+#print seqs
+
+genome=readgenome('resources/lambda_virus.fa')
+pattern='AGGAGGTT'
+t=reversecomplement(pattern)
+occurences=naive2mm(pattern,genome)
+print "pattern={}   t={}".format(repr(pattern),repr(t))
+
+        
